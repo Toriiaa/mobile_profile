983b57e765ef7537e2e9175aaa5d2ec0293d0251
toriiaa <meritoriaghe@gmail.com>
2021-05-02 23:20:52 +0100

    new commits

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,61 @@ import 'package:flutter/material.dart';
 
 void main() =>runApp(MyApp());
 
-class MyApp  extends StatelessWidget{
-   @override
+class MyApp extends  StatelessWidget {
+  
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Toriia Profile"),
-        )
-      ),
-
-
-
-    ),
+      home: MyDesign()
+    );
+    // Home ends here
   }
 }
+ class MyDesign extends StatefulWidget {
+  
 
- 
- 
+  @override
+  MyDesignState createState() => MyDesignState();
+}
+
+class MyDesignState extends State <MyDesign> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(" Toriia Profile")
+        ),
+        // end of appbar
+        body: Container(
+          decoration: BoxDecoration(
+            // color: Colors.blue,
+          ),
+          // end of boxdecoration
+          child: Column(
+            
+              children: <Widget>[
+              SizedBox(
+                height: 100
+              ),
+              // sizebox
+              CircleAvatar(
+                radius:30,
+                child: Image.asset("merit.jpeg")
+              ),
+              // circle avatar
 
 
+              ]
+            
+
+          ),
+          // end of column
+
+        ),
+        // end of container
+      ); 
+      // scaffold end
+  }
+} 
+  
