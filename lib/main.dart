7b65929a0c15259840ba592ef9833cd7d360ff18
toriiaa <meritoriaghe@gmail.com>
2021-05-13 17:34:46 +0100

    new commits

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,97 @@
 import 'package:flutter/material.dart';
- 
 
-void main() =>runApp(MyApp());
+void main() => runApp(MyApp());
 
-class MyApp extends  StatelessWidget {
-  
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyDesign()
-    );
+    return MaterialApp(home: MyDesign());
     // Home ends here
   }
 }
- class MyDesign extends StatefulWidget {
-  
 
+class MyDesign extends StatefulWidget {
   @override
   MyDesignState createState() => MyDesignState();
 }
 
-class MyDesignState extends State <MyDesign> {
+class MyDesignState extends State<MyDesign> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(" Toriia Profile")
+      appBar: AppBar(title: Text(" Toriia Profile")),
+      // end of appbar
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.blue,
         ),
-        // end of appbar
-        body: Container(
-          decoration: BoxDecoration(
-            color: Colors.blue,
+        // end of boxdecoration
+        child: Column(children: <Widget>[
+          SizedBox(height: 100),
+          // sizebox
+          CircleAvatar(radius: 100, child: Image.asset("images/girl.jpg")),
+          // circle avatar
+
+          SizedBox(
+            height: 20,
           ),
-          // end of boxdecoration
-          child: Column(
-            
-              children: <Widget>[
-
-              SizedBox(
-                height: 100
-              ),
-              // sizebox
-              CircleAvatar(
-                radius:100,
-                child: Image.asset("images/merit.jpeg") 
-                
-              ),
-              // circle avatar
-
-              SizedBox(
-              height: 20,
-              ),
-              // sized box
-              Text("Merit Oriaghemuoria"),
-              // text
-
-               SizedBox(
-              height: 20,
-              ),
-              // sized box
-              Text("@techEntusiast"),
-              // text
-
-               SizedBox(
-              height: 20,
-              ),
-              // sized box
-              Text("#flutterdeveloper"),
-              // text
-               SizedBox(
-              height: 40,
-              ),
-              // sized box
-              Text("Merit is a talented front end developer who specializes in converting your web projects into mobile applications"),
-              // text
-
-
-
-              ]
-            
-
+          // sized box
+          Text(
+            "Merit Oriaghemuoria",
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.white,
+              fontWeight: FontWeight.w900,
+            ),
           ),
-          // end of column
 
-        ),
-        // end of container
-      ); 
-      // scaffold end
+          // text
+
+          SizedBox(
+            height: 20,
+          ),
+          // sized box
+          Text(
+            "@techEnthusiast",
+            style: TextStyle(
+              fontSize: 15,
+              color: Colors.white,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          // text
+
+          SizedBox(
+            height: 20,
+          ),
+          // sized box
+          Text(
+            "#flutterdeveloper",
+            style: TextStyle(
+              fontSize: 10,
+              color: Colors.white,
+              fontWeight: FontWeight.w300,
+            ),
+          ),
+          // text
+          SizedBox(
+            height: 40,
+          ),
+          // sized box
+          Text(
+            "Merit is a talented front end developer who specializes in converting your web projects into mobile applications",
+            style: TextStyle(
+              fontSize: 15,
+              color: Colors.white,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          // text
+        ]),
+        // end of column
+      ),
+      // end of container
+    );
+    // scaffold end
   }
-} 
-  
+}
